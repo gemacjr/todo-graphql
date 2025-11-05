5365126ba9785859b1033c1b7a8ac4fa1770d798
gemacjr <edmccormic@me.com>
2025-11-05 09:40:25 -0500

    intial commit

---
+++ src/main/java/com/swiftbeard/todo_graphql/TodoGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.todo_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftbeard/todo_graphql/TodoGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.todo_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
